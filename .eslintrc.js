5063142b7624bd637690f161c3935cab84180ba8
Tony Jin <tony.jin@moneysmart.com>
2020-03-31 01:21:44 +0800

    May the force be with you

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ["standard"],
+  parser: "babel-eslint",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    "max-len": ["error", { code: 90 }]
+  }
+};
